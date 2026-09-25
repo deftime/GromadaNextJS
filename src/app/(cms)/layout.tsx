1c87f4b1373ab03014fcdb72c059ceb6e5575f3b
Default <deftime@gmail.com>
2026-09-25 16:40:40 +0300

    Admin CMS. Base structure, sidebar, navlinks.

--- src/app/(cms)/layout.tsx
+++ src/app/(cms)/layout.tsx
@@ -1,4 +1,8 @@
 import { Metadata } from "next";
+import Link from "next/link";
+import homeLogo from '@/assets/home.svg';
+import formLogo from '@/assets/form.svg';
+import panelLogo from '@/assets/dashboard.svg';
 import '@/styles/admin/general.scss';
 
 export const metadata: Metadata = {
@@ -9,9 +13,26 @@ export const metadata: Metadata = {
 export default function Layout({ children }: { children: React.ReactNode}) {
   return (
     <section className="admin-layout">
-      <div className="admin-top"></div>
+      <div className="admin-top">
+        <Link href="/" className="site-link" target='_blank'>
+          <img src={homeLogo.src} alt="Home logo" />
+          <span>To site</span>
+        </Link>
+      </div>
       <div className="admin-base">
         <div className="sidebar">
+
+          {/* Component? */}
+          <Link href="/admin" className="admin-link">
+            <img src={panelLogo.src} alt="Admin page icon" />
+            <span>Panel</span>
+          </Link>
+
+          {/* Component? */}
+          <Link href="/admin/feedback-form" className="admin-link">
+            <img src={formLogo.src} alt="Admin page icon" />
+            <span>Feedback form</span>
+          </Link>
 
         </div>
         <div className="page-zone">
